b2675879245c5144d55f79fde51257c449b9e81e
Daniel Rivera Guzman <drivera1@uabc.edu.mx>
2016-05-08 14:20:29 -0700

    Uso de AnchorScroll para desplazarnos sobre la pagina mediante enlaces y hash para indicar el id en nuestra url

--- public/javascript/controllers/AnchorExampleCtrl.js
+++ public/javascript/controllers/AnchorExampleCtrl.js
@@ -8,12 +8,18 @@
         .module('myApp')
         .controller('AnchorCtrl', AnchorCtrl);
 
-    AnchorCtrl.$inject = ['$scope', '$anchorScroll'];
+    AnchorCtrl.$inject = ['$scope', '$anchorScroll', '$location'];
 
     /* @ngInject */
-    function AnchorCtrl($scope, $anchorScroll) {
+    function AnchorCtrl($scope, $anchorScroll, $location) {
+        //Obtenemos la Id que pasamos de la vista mediante la funcion
+        //irA, con ella creamos un identificador haremos el scroll hacia
+        //la seccion de la pagina con el id que seleccionamos
         $scope.irA = function (id) {
             var nuevaId = 'contenido' + id;
+            //Mediante .hash() podemos agregar la id en nuestra url
+            $location.hash(nuevaId);
+            //Indicamos hacia cual id se movera
             $anchorScroll(nuevaId);
         }
     }
