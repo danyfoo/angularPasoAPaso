7fb286fb7446556ac4afc5ef3d0d5547d75bc992
Daniel Rivera Guzman <drivera1@uabc.edu.mx>
2016-05-09 00:38:50 -0700

    Uso del metodo $http.get para unicamente obtener algun dato del servidor

--- public/javascript/controllers/UserCtrl.js
+++ public/javascript/controllers/UserCtrl.js
@@ -14,15 +14,14 @@
     function UserCtrl($scope, $http) {
         var vm = this;
         vm.title = 'UserCtrl';
-
-        var usuarios = $http({
-            method: 'GET',
-            url: 'http://jsonplaceholder.typicode.com/users'
-        }).success(function (data, status) {
-            $scope.usuarios = data;
-        }).error(function (data, status) {
-            console.log(data, status);
-        });
+        
+        $http.get('http://jsonplaceholder.typicode.com/users')
+            .success(function (data, status) {
+                $scope.usuarios = data;
+            })
+            .error(function (data, status) {
+                console.log(data, status);
+            })
     }
 
 })();
